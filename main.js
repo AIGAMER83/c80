48cfbf87d8b46450ee8fa56f510220ac2f3075bb
AIGAMER83 <80625185+AIGAMER83@users.noreply.github.com>
2021-04-06 10:18:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function getParagraphs1(){
+    }
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++){
+        inputs.push(document.getElementById("para1_input_box_").value);
+inputs.join(".")
+document.getElementsById("showParagraph1").innerHTML = inputs.join(".");    
+}
+function getParagraphs2(){
+}
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++){
+    inputs.push(document.getElementById("para1_input_box_").value);
+inputs.join(".")
+document.getElementsById("showParagraph2").innerHTML = inputs.join(".");    
+}
+function getParagraphs3(){
+}
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++){
+    inputs.push(document.getElementById("para1_input_box_").value);
+inputs.join(".")
+document.getElementsById("showParagraph3").innerHTML = inputs.join(".");    
+}
+function getParagraphs4(){
+}
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++){
+    inputs.push(document.getElementById("para1_input_box_").value);
+inputs.join(".")
+document.getElementsById("showParagraph4").innerHTML = inputs.join(".");    
+}
+function getParagraphs5(){
+}
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++){
+    inputs.push(document.getElementById("para1_input_box_").value);
+inputs.join(".")
+document.getElementsById("showParagraph5").innerHTML = inputs.join(".");    
+}
+function getParagraphs6(){
+}
+var inputs = [];
+for(var i = 1 ; i <=6 ; i++){
+    inputs.push(document.getElementById("para1_input_box_").value);
+inputs.join(".")
+document.getElementsById("showParagraph6").innerHTML = inputs.join(".");    
+}
